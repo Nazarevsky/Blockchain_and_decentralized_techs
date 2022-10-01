f9c56a456ce166599d87f800f0056d3f1145d3b7
Nazarii_Shcherbak <nazarevsky.ua@gmail.com>
2022-10-01 13:46:53 +0300

    Пз-1

--- main.go
+++ main.go
@@ -18,11 +18,11 @@ var keyCount = make(map[int]string)
 var bits []int = []int{8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096}
 
 func keyHandler(key string) {
-	if key == "1" {
+	if key == "0" {
 		task1()
-	} else if key == "2" {
+	} else if key == "1" {
 		task2()
-	} else if key == "3" {
+	} else if key == "2" {
 		task3()
 	} else if key == "*" {
 		execution = false
@@ -57,12 +57,12 @@ func task1() {
 	for true {
 		if _, exist := keyCount[8]; exist {
 			for i := 0; i < len(bits); i++ {
-				fmt.Println(strconv.FormatInt(int64(bits[i]), 10) + "-біт: " + keyCount[bits[i]])
+				fmt.Println(strconv.FormatInt(int64(bits[i]), 10) + " біт: " + keyCount[bits[i]])
 			}
 		} else {
 			fillKeyCount()
 			for i := 0; i < len(bits); i++ {
-				fmt.Println(strconv.FormatInt(int64(bits[i]), 10) + "-біт: " + keyCount[bits[i]])
+				fmt.Println(strconv.FormatInt(int64(bits[i]), 10) + " біт: " + keyCount[bits[i]])
 			}
 		}
 
@@ -76,7 +76,6 @@ func task1() {
 
 func task2() {
 	var task2Key string
-	rand.Intn(100)
 
 	if _, exist := keyCount[8]; !exist {
 		fillKeyCount()
@@ -91,7 +90,7 @@ func task2() {
 			}
 
 			val.Rand(rand.New(rand.NewSource(int64(time.Now().UnixMilli()))), val)
-			fmt.Println(strconv.FormatInt(int64(bits[i]), 10) + "-біт значення: " + val.String())
+			fmt.Println(strconv.FormatInt(int64(bits[i]), 10) + " біт значення: " + val.String())
 		}
 
 		fmt.Println("Будь-яка кнопка. Згенерувати нові значення.")
@@ -103,9 +102,41 @@ func task2() {
 	}
 }
 
+func bruteForce(bitCount int) {
+	var bruteForceKey string
+	if _, exist := keyCount[8]; !exist {
+		fillKeyCount()
+	}
+
+	randNumMax, err := big.NewInt(0).SetString(keyCount[bitCount], 10)
+	randNum := big.NewInt(randNumMax.Int64())
+	if !err {
+		panic(err)
+	}
+	for true {
+		ClearConsole()
+		randNum.Rand(rand.New(rand.NewSource(int64(time.Now().UnixMilli()))), randNumMax)
+		fmt.Println("Шукається значення (" + randNum.String() + ")...")
+
+		start := time.Now().UnixMilli()
+		i := big.NewInt(0)
+		for ; i.Cmp(randNum) < 0; i.Add(i, big.NewInt(1)) {
+		}
+		end := time.Now().UnixMilli()
+
+		fmt.Println("Значення знайдено: " + i.String() + " знайдено за " + strconv.FormatInt(end-start, 10) + " мс.")
+		fmt.Println("Будь-яка кнопка. Згенерувати нове значення.")
+		fmt.Println("*. Повернутися у меню.")
+		fmt.Scanln(&bruteForceKey)
+		if bruteForceKey == "*" {
+			break
+		}
+	}
+}
+
 func task3() {
 	ClearConsole()
-	var task1Key string
+	var task3Key string
 
 	for true {
 		ClearConsole()
@@ -116,9 +147,11 @@ func task3() {
 		}
 
 		fmt.Println("*. Повернутися у меню.")
-		fmt.Scanln(&task1Key)
-		if task1Key == "*" {
+		fmt.Scanln(&task3Key)
+		if task3Key == "*" {
 			break
+		} else if task3Key[0] >= 48 && task3Key[0] <= 57 {
+			bruteForce(bits[task3Key[0]%48])
 		}
 	}
 }
@@ -127,13 +160,12 @@ func main() {
 	opSys = runtime.GOOS
 	execution = true
 	var key string
-
 	for execution {
 		ClearConsole()
 		fmt.Println("Оберіть завдання та нажміть потрібну цифру.")
-		fmt.Println("1. Завдання 1: Кількість варіантів ключів.")
-		fmt.Println("2. Завдання 2: Випадкові значення ключів.")
-		fmt.Println("3. Завдання 3: Брутфорс.")
+		fmt.Println("0. Завдання 1: Кількість варіантів ключів.")
+		fmt.Println("1. Завдання 2: Випадкові значення ключів.")
+		fmt.Println("2. Завдання 3: Брутфорс.")
 		fmt.Println("*. Вийти з програми.")
 
 		fmt.Scanln(&key)
