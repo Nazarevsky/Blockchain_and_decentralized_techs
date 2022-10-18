04b77a5716512c3d4e1a551839bf1392f18abee0
Nazarii_Shcherbak <nazarevsky.ua@gmail.com>
2022-10-18 22:37:15 +0300

    Practise 2

--- Practise_2-Hex_convertation/main.go
+++ Practise_2-Hex_convertation/main.go
@@ -2,29 +2,34 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"strconv"
+	"math/big"
 )
 
-func hexToDecimal(hex string) {
-	strconv.ParseInt(hex, 16, 64)
+func hexToLittleEndian(value string) *big.Int {
+	valueChars := []rune(value)
+	for i := 2; i < len(value)/2; i += 2 {
+		valueChars[len(value)-i], valueChars[i] = valueChars[i], valueChars[len(value)-i]
+		valueChars[len(value)-i+1], valueChars[i+1] = valueChars[i+1], valueChars[len(value)-i+1]
+	}
+	answ, err := big.NewInt(0).SetString(string(valueChars)[2:], 16)
+
+	if !err {
+		panic("Помилка! Конвертується не число.")
+	}
+	return answ
 }
 
-func hexToLittleEndian(value string) int64 {
-	var answer int64
-	for i := 2; i < len(value); i++ {
-		num, err := strconv.ParseInt(string(value[i]), 16, 64)
-		if err != nil {
-			panic(err)
-		}
-
-		answer += num * int64(math.Pow(16, float64(i-2)))
+func hexToBigEndian(value string) *big.Int {
+	answer, err := big.NewInt(0).SetString(value, 16) // remake 0x should not get in!
+	if !err {
+		panic("Error when converting!")
 	}
+
 	return answer
 }
 
 func main() {
-	str := "0xff00000000000000000000000000000000000000000000000000000000000000"
+	str := "0x0d0f000000000000000000000000000000000000000000000000000000000000"
 	answ := hexToLittleEndian(str)
-	fmt.Println("Answer: " + strconv.FormatInt(answ, 10))
+	fmt.Println("Answer: " + answ.String())
 }
