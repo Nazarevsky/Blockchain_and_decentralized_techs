e20cbb6b4899ced35b5f914e4dc43253293206d9
Nazarii_Shcherbak <nazarevsky.ua@gmail.com>
2022-10-19 10:37:58 +0300

    Practise 2

---
+++ Practise_2-Hex_convertation/endian.go
@@ -0,0 +1,68 @@
+package endian
+
+// if we write a library, we can't use 0x at the beginning!!
+import (
+	"fmt"
+	"math"
+	"math/big"
+	"strings"
+)
+
+func addZeroToLen(str string, length int, state bool) string {
+	concat := strings.Repeat("0", length-len(str))
+
+	if state {
+		return str + concat
+	} else {
+		return concat + str
+	}
+}
+
+func reverseHexNum(value string) string { // startNum if we have 0x at the beginning
+	valueChars := []rune(value)
+	valLen := int(math.Round(float64(len(value)) / float64(2)))
+	for i := 0; i < valLen; i += 2 {
+		valueChars[len(value)-i-2], valueChars[i] = valueChars[i], valueChars[len(value)-i-2]
+		valueChars[len(value)-i-1], valueChars[i+1] = valueChars[i+1], valueChars[len(value)-i-1]
+	}
+
+	return string(valueChars)
+}
+
+func hexToLittleEndian(value string) *big.Int {
+	revStr := reverseHexNum(value)
+	answ, err := big.NewInt(0).SetString(revStr, 16)
+
+	if !err {
+		panic("Помилка! Конвертується не число.")
+	}
+	return answ
+}
+
+func hexToBigEndian(value string) *big.Int {
+	answer, err := big.NewInt(0).SetString(value, 16) // remake 0x should not get in!
+	if !err {
+		panic("Помилка! Конвертується не число.")
+	}
+
+	return answer
+}
+
+func bigintToHex(n *big.Int) string {
+	str := fmt.Sprintf("%x", n)
+	length := 32
+	for ; len(str) > length; length *= 2 {
+	}
+
+	return addZeroToLen(str, length, true)
+}
+
+func littleEndianToHex(value *big.Int) string {
+	hexVal := bigintToHex(value)
+	hexVal = reverseHexNum(hexVal)
+	return hexVal
+}
+
+func bigEndianToHex(value *big.Int) string {
+	return fmt.Sprintf("%x", value)
+}
