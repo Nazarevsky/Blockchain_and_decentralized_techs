de114966f50d9a72504e247e1e549bfd81719ff3
Nazarii_Shcherbak <nazarevsky.ua@gmail.com>
2022-10-31 19:46:05 +0200

    Practise 3

--- Practise_3-Hashing_algorithms/hashing/hashAlgos.go
+++ Practise_3-Hashing_algorithms/hashing/hashAlgos.go
@@ -1,5 +1,32 @@
 package hashing
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+var sha1_const = []int{0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0}
+
+func binary(s string) string {
+	res := ""
+	for _, c := range s {
+		res = fmt.Sprintf("%s%.8b", res, c)
+	}
+	return res
+}
+
+func addZerosTo(mes string, to int) string {
+	return strings.Repeat("0", to-len(mes)) + mes
+}
+
+func filler(binMes string) string {
+	length := len(binMes)
+	binLen := binary(strconv.Itoa(length))
+	return binMes + "1" + strings.Repeat("0", 512-((length+65)%512)) + addZerosTo(binLen, 64)
+}
+
 func SHA1(message string) string {
-	return message
+	binForm := binary(message)
+	return filler(binForm)
 }
--- Practise_3-Hashing_algorithms/main.go
+++ Practise_3-Hashing_algorithms/main.go
@@ -5,7 +5,10 @@ import (
 	"pract3/hashing"
 )
 
+var sha1_const = []int{0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0}
+
 func main() {
-	message := "Some text"
+	message := "I can literally talk all dayI can literally talk all dayI can literally talk all dayI can literally talk all dayI can literally talk all dayI can literally talk all dayI can literally talk all dayI can literally talk all dayI can literally talk all day"
+	fmt.Println(len(message))
 	fmt.Println(hashing.SHA1(message))
 }
