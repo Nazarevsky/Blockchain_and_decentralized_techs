05af68e7faafb7d6738daac6dea365479b93925e
Nazarii_Shcherbak <nazarevsky.ua@gmail.com>
2022-10-31 21:00:07 +0200

    Practise 3

--- Practise_3-Hashing_algorithms/hashing/hashAlgos.go
+++ Practise_3-Hashing_algorithms/hashing/hashAlgos.go
@@ -6,7 +6,8 @@ import (
 	"strings"
 )
 
-var sha1_const = []int{0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0}
+var sha1_h = []int{0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0}
+var w []int64
 
 func binary(s string) string {
 	res := ""
@@ -28,5 +29,22 @@ func filler(binMes string) string {
 
 func SHA1(message string) string {
 	binForm := binary(message)
+	binForm = filler(binForm)
+
+	for i := 0; i < len(binForm)%512; i += 0 {
+		chunk := binForm[i*512 : i*512+512]
+		for i := 0; i < 16; i++ {
+			num, _ := strconv.ParseInt(chunk[i*32:i*32+32], 2, 64)
+			w = append(w, num)
+		}
+
+		for i := 16; i < 80; i++ {
+			w = append(w, (w[i-3]^w[i-8]^w[i-14]^w[i-16])<<1) // or left shift by 5????
+		}
+
+		arr := make([]int, len(sha1_h))
+		copy(arr, sha1_h)
+	}
+
 	return filler(binForm)
 }
--- Practise_3-Hashing_algorithms/main.go
+++ Practise_3-Hashing_algorithms/main.go
@@ -5,8 +5,6 @@ import (
 	"pract3/hashing"
 )
 
-var sha1_const = []int{0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0}
-
 func main() {
 	message := "I can literally talk all dayI can literally talk all dayI can literally talk all dayI can literally talk all dayI can literally talk all dayI can literally talk all dayI can literally talk all dayI can literally talk all dayI can literally talk all day"
 	fmt.Println(len(message))
