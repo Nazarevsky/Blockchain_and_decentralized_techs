34a7d0d223fb88daf6549aace4207057fb148ffa
Nazarii_Shcherbak <nazarevsky.ua@gmail.com>
2022-11-22 23:31:04 +0200

    Testing el curve methods

--- Practise_5-Baby_blockchain/ecdsa/keypair.go
+++ Practise_5-Baby_blockchain/ecdsa/keypair.go
@@ -106,9 +106,9 @@ func isPointEqual(p1, p2 Point) bool {
 }
 
 func add(p1, p2 Point) Point {
-	// if isPointEqual(p1, p2) { // try to check this: remove and run. If similar, it`s correct
-	// 	return double(p1)
-	// }
+	if isPointEqual(p1, p2) { // try to check this: remove and run. If similar, it`s correct
+		return double(p1)
+	}
 	var res Point
 
 	// slope
@@ -169,15 +169,15 @@ var _p int = 47
 
 func mod(a, m int) int {
 	if a < 0 {
-		return (a * -1) % m
+		return m - ((a * -1) % m)
 	}
 	return 1 % m
 }
 
 func dbl(p P) P {
-	slope := ((3*p.x*p.x + a1) * inv(2*p.y, _p)) % _p
-	x := (slope*slope - (2 * p.x)) % _p
-	y := (slope*(p.x-x) - p.y) % _p
+	slope := mod(((3*p.x*p.x + a1) * inv(2*p.y, _p)), _p)
+	x := mod((slope*slope - (2 * p.x)), _p)
+	y := mod((slope*(p.x-x) - p.y), _p)
 	var res P
 	res.x = x
 	res.y = y
@@ -185,9 +185,9 @@ func dbl(p P) P {
 }
 
 func ad(p1, p2 P) P {
-	slope := ((p1.y - p2.y) * inv(p1.x-p2.x, _p)) % _p
-	x := (slope*slope - p1.x - p2.x) % _p
-	y := ((slope * (p1.x - x)) - p1.y) % _p
+	slope := mod(((p1.y - p2.y) * inv(p1.x-p2.x, _p)), _p)
+	x := mod((slope*slope - p1.x - p2.x), _p)
+	y := mod(((slope * (p1.x - x)) - p1.y), _p)
 	var res P
 	res.x = x
 	res.y = y
@@ -197,12 +197,14 @@ func ad(p1, p2 P) P {
 func GenPublKey() { //key *big.Int
 	a = big.NewInt(0)
 	a = big.NewInt(7)
-	p, _ = big.NewInt(0).SetString("115792089237316195423570985008687907853269984665640564039457584007908834671663", 10)
-	n, _ = big.NewInt(0).SetString("115792089237316195423570985008687907852837564279074904382605163141518161494337", 10)
-	x, _ := big.NewInt(0).SetString("55066263022277343669578718895168534326250603453777594175500187360389116729240", 10)
-	y, _ := big.NewInt(0).SetString("32670510020758816978083085130507043184471273380659243275938904335757337482424", 10)
-	G.x = x
-	G.y = y
+	p = big.NewInt(47)
+	n = big.NewInt(10)
+	//p, _ = big.NewInt(0).SetString("115792089237316195423570985008687907853269984665640564039457584007908834671663", 10)
+	//n, _ = big.NewInt(0).SetString("115792089237316195423570985008687907852837564279074904382605163141518161494337", 10)
+	// x, _ := big.NewInt(0).SetString("55066263022277343669578718895168534326250603453777594175500187360389116729240", 10)
+	// y, _ := big.NewInt(0).SetString("32670510020758816978083085130507043184471273380659243275938904335757337482424", 10)
+	// G.x = x
+	// G.y = y
 
 	var a Point
 	a.x = big.NewInt(1)
@@ -212,18 +214,21 @@ func GenPublKey() { //key *big.Int
 	b.x = big.NewInt(1)
 	b.y = big.NewInt(2)
 
-	//c := add(a, b)
-	// println(c.x.String())
-	// println(c.y.String())
+	c := add(a, b)
+	println(c.x.String(), c.y.String())
+	c = double(a)
+	println(c.x.String(), c.y.String())
 
 	var a_ P
 	a_.x = 1
 	a_.y = 2
 
 	var b_ P
-	a_.x = 1
-	a_.y = 2
+	b_.x = 1
+	b_.y = 2
 
 	r := ad(a_, b_)
+	println(r.x, r.y)
+	r = dbl(a_)
 	println(r.x, r.y)
 }
